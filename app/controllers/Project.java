d5a8cb4dfd8ea79bc47161d39369e5e10d346263
Jivings <james@ivings.org.uk>
2012-06-22 12:28:30 +0100

    Forms now work. All project details committed to the database when user clicks save.

--- app/controllers/Project.java
+++ app/controllers/Project.java
@@ -2,14 +2,17 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-import play.data.Form;
+import play.data.*;
 
 import views.html.*;
 import models.*;
 
+import java.security.SecureRandom;
+
 public class Project extends Controller {
 
   public static Form<ProjectModel> projectForm = form(ProjectModel.class);
+
   /**
    * Get the default project
    */
@@ -21,9 +24,12 @@ public class Project extends Controller {
    * Get a saved Project
    */
   public static Result get(String uuid) {
+    // get project from the database
     ProjectModel p = ProjectModel.find.byId(uuid);
-    return ok(index.render(p, projectForm)); // GET views.index.html
+    // fill the form model and display
+    return ok(index.render(projectForm.fill(p), p)); // GET views.index.html
   }
+
   /**
    * Get a project revision
    */
@@ -43,10 +49,18 @@ public class Project extends Controller {
    * Create a new Project
    */
   public static Result newProject() {
-    ProjectModel p = new ProjectModel();
-    
+    // fill the form Object with POST values
+    Form<ProjectModel> filledForm = projectForm.bindFromRequest();
+    // construct the model
+    ProjectModel p = filledForm.get();
+    // create a Unique Identifier
+    p.uuid = createUUID();    
+    // save to the database
     p.save();
-    return ok("new");
+    return redirect("/"+p.uuid);
+  }
+
+  private static String compile() {
   }
 
   public static Result debug() {
@@ -60,5 +74,36 @@ public class Project extends Controller {
       "}";
     p.save();
     return ok("Debug sample created");
+  }
+
+  private static SecureRandom random = new SecureRandom();
+
+  
+  static char[] available = new char[52];
+  // initialise values for UUID
+  static {
+    int idx = 0, upper = 65, lower = 97;
+    for (int x = upper; x < 123; x++, idx++) {
+      if (x == 91) {
+        x = lower;
+      }
+      available[idx] = (char) x;
+    }
+  }
+
+  /**
+   * Create a Unique Identifier from the available 
+   * chars.
+   * This gives 52^6 different values.
+   *
+   * BEWARE: Birthday paradox
+   * TODO: Check that value doesn't exist.
+   */
+  public static String createUUID() {
+    final char[] id = new char[6];
+    for (int i = 0; i < id.length; i++) {
+      id[i] = available[random.nextInt(52)];
+    }
+    return new String(id);
   }
 }
